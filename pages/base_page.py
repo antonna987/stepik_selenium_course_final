50f9e97c88cbe9bd54c66257c5ffc67ff90baa4d
Anton Nazin <anton.na987@gmail.com>
2022-10-16 01:27:06 +0300

    add login tests for product page

--- pages/base_page.py
+++ pages/base_page.py
@@ -5,6 +5,7 @@ from selenium.common.exceptions import (
 )
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions
+from .locators import BasePageLocators
 import math
 
 
@@ -16,6 +17,15 @@ class BasePage:
 
     def open(self):
         self.browser.get(self.url)
+
+    def go_to_login_page(self):
+        link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
+        link.click()
+
+    def should_be_login_link(self):
+        assert self.is_element_present(
+            *BasePageLocators.LOGIN_LINK
+        ), "Login link is not presented"
 
     def is_element_present(self, how, what):
         try:
--- test_product_page.py
+++ test_product_page.py
@@ -1,6 +1,22 @@
+from .pages.login_page import LoginPage
 from .pages.product_page import ProductPage
 import pytest
 import time
+
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    LoginPage(browser, link).should_be_login_page()
 
 
 @pytest.mark.xfail
