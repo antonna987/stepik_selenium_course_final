164869348c09e6616409a848f7bd559750a253ba
Anton Nazin <anton.na987@gmail.com>
2022-10-15 23:42:08 +0300

    add login page tests

--- pages/locators.py
+++ pages/locators.py
@@ -3,3 +3,12 @@ from selenium.webdriver.common.by import By
 
 class MainPageLocators:
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+
+class LoginPageLocators:
+    LOGIN_EMAIL = (By.CSS_SELECTOR, "#id_login-username")
+    LOGIN_PASSWORD = (By.CSS_SELECTOR, "#id_login-password")
+
+    REGISTER_EMAIL = (By.CSS_SELECTOR, "#id_registration-email")
+    REGISTER_PASSWORD = (By.CSS_SELECTOR, "#id_registration-password1")
+    REGISTER_CONFIRM_PASSWORD = (By.CSS_SELECTOR, "#id_registration-password2")
--- test_main_page.py
+++ test_main_page.py
@@ -1,3 +1,4 @@
+from .pages.login_page import LoginPage
 from .pages.main_page import MainPage
 from selenium.webdriver.common.by import By
 
@@ -7,6 +8,7 @@ def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link)
     page.open()
     page.go_to_login_page()
+    LoginPage(browser, link).should_be_login_page()
 
 
 def test_guest_should_see_login_link(browser):
