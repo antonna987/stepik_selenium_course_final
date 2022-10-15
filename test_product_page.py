047271c61f9c277012a03e8204f72a39e5b2f957
Anton Nazin <anton.na987@gmail.com>
2022-10-16 02:44:11 +0300

    mark tests with need_review

--- test_product_page.py
+++ test_product_page.py
@@ -24,6 +24,7 @@ class TestUserAddToBasketFromProductPage:
         page.open()
         page.should_not_be_success_message()
 
+    @pytest.mark.need_review
     def test_user_can_add_product_to_basket(self, browser):
         link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0"
         page = ProductPage(browser, link)
@@ -36,6 +37,7 @@ class TestUserAddToBasketFromProductPage:
         page.basket_total_should_be(product_price)
 
 
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
@@ -52,6 +54,7 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page.should_be_login_link()
 
 
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
@@ -87,6 +90,7 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     page.success_message_should_disappear()
 
 
+@pytest.mark.need_review
 @pytest.mark.parametrize(
     "link",
     [
